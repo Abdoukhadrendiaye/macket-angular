b5ff2fd82946407b70f6cfb0c6233ad21d983b90
Abdoukhadrendiaye <djilyndiayekhadre@gmail.com>
2024-10-28 13:58:31 +0000

    les pages du macket decommenter le content pour afficher le dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { SidenavComponent } from "./sidenav/sidenav.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, SidebarComponent, SidenavComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'macket-angular';
+
+    categorie=[
+        {
+            id:1,
+            model:"model",
+            marque:"toyota",
+            date:"12/02/22",
+            types:"depanage",
+            voiture:498
+        },
+        {
+           id:2,
+            model:"model",
+            marque:"toyota",
+            date:"12/02/22",
+            types:"depanage",
+            voiture:503 
+        },
+        {
+            id:3,
+            model:"model",
+            marque:"toyota",
+            date:"12/02/22",
+            types:"depanage",
+            voiture:507
+        }
+    ]
 }
+
